9fa583664f1033f85a94dd26b6a68da8f2178b26
rottencarrot33 <122114827+rottencarrot33@users.noreply.github.com>
2024-03-19 17:30:55 +0100

    Update form.php

--- form.php
+++ form.php
@@ -15,4 +15,23 @@
       echo "There was a problem sending the message.";
     }
   }
+
+    
+$secretKey = "6LfDK4YpAAAAAAEiOSMhWs1fmnARuE5kyjHK51F0";
+$responseKey = $_POST['g-recaptcha-response'];
+$userIP = $_SERVER['REMOTE_ADDR'];
+
+$url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$userIP";
+$response = file_get_contents($url);
+$responseKeys = json_decode($response, true);
+
+if (intval($responseKeys["success"]) !== 1) {
+    // CAPTCHA verification failed
+    // Handle the error here, maybe show an error message to the user
+} else {
+    // CAPTCHA verification passed
+    // Proceed with your form submission
+}
+
+
 ?>
